36e5f8a8d90aa9667da8b651f3a27b11b434a01f
Femi Adeniran <66181395+phenomenon0@users.noreply.github.com>
2024-06-04 13:06:24 -0500

    Lean and clean

--- pages/Alpha.py
+++ pages/Alpha.py
@@ -3,6 +3,8 @@ from openai import OpenAI
 import json
 import streamlit as st
 import os 
+import random
+
 st.markdown('## Alpha')
 st.sidebar.markdown('This is the Alpha page')
 
@@ -66,7 +68,7 @@ def get_scouting_report(_llm, model, player, temperature, p,seed):
             },
             {
                 "role": "user",
-                "content": player_boxscore + "analyze player"
+                "content": player_boxscore + "take a deep breath analyze the  player and follow the example for grammar,language and tone use always use stats one each line to  highlight your point. short and to the point "
             },
             
         ],
@@ -75,10 +77,21 @@ def get_scouting_report(_llm, model, player, temperature, p,seed):
     return chat_completion.choices[0].message.content
 #openai strategy is to tweak either temperature or top_p to get the desired output not both 
 seed_value = st.radio("Seed Values", [111, 22, 23, 4])
-temperature = st.slider("Temperature", min_value=0.1, max_value=2.0, value=0.5, step=0.1)
+temperature = st.slider("Temperature", min_value=0.7, max_value=1.8, value=0.8, step=0.1)
 top_p = st.slider("Top P", min_value=0.1, max_value=1.0, value=0.9, step=0.1)
 
 if st.button("Generate Scouting Report"):
-    scouting_report = get_scouting_report(client,  "gpt-3.5-turbo", player_boxscore,temperature, top_p, seed=seed_value)
+    scouting_report = get_scouting_report(client,  "gpt-4o", player_boxscore,temperature, top_p, seed=seed_value)
     st.write(scouting_report)
     
+sample_data = [
+    """3 • Chuma Okeke • F • 6'7" • 229 lbs GP-GS PTS MIN FGM-A FG% 3PM-A 3P% FT% FTM-A REB AST TO STL BLK Regular Season 42-8 2.5 9.8 0.9-2.5 36.4% 0.5-1.9 29.1% 40.0% 0.0-0.1 1.8 0.5 0.3 0.2 0.2 Spot Up 46.3% 57 44 0.772 - - 39 15 54 27.8% 40.7% 5.3% 0% 0% 26.3% P&R Ball Handler 3.3% 4 7 1.75 - - 1 3 4 75% 75% 0% 25% 25% 75% Transition 13% 16 17 1.062 - - 6 6 12 50% 70.8% 25% 0% 0% 37.5% Cut 6.5% 8 10 1.25 - - 3 5 8 62.5% 62.5% 0% 0% 0% 62.5% Isolation 2.4% 3 3 1 - - 1 1 2 50% 75% 33.3% 0% 0% 33.3% P&R Roll Man 3.3% 4 3 0.75 - - 3 1 4 25% 37.5% 0% 0% 0% 25% Offensive Rebounds (put backs) 4.9% 6 4 0.667 - - 2 2 4 50% 50% 16.7% 16.7% 16.7% 33.3% Off Screen 4.9% 6 3 0.5 - - 5 1 6 16.7% 25% 0% 0% 0% 16.7% Hand Off 10.6% 13 11 0.846 - - 5 5 10 50% 55% 23.1% 0% 0% 38.5% Miscellaneous 4.9% 6 1 0.167 - - 3 0 3 0% 0% 33.3% 16.7% 0% 16.7%""",
+    """11 • Klay Thompson • G • 6'6" • 220 lbs GP-GS PTS MIN FGM-A FG% 3PM-A 3P% FT% FTM-A REB AST TO STL BLK Regular Season 78-64 17.6 29.7 6.3-14.7 42.9% 3.4-8.9 38.4% 92.7% 1.6-1.8 3.3 2.3 1.5 0.6 0.4Play Type %Time Poss Points PPP %Rank Rating FGm FGM FGA FG% aFG% %TO %FT %SF %Score Off Screen 32.9% 431 472 1.095 - - 221 162 383 42.3% 56.5% 7.4% 5.3% 5.3% 41.3% Spot Up 17.7% 232 250 1.078 - - 137 90 227 39.6% 54% 1.7% 1.7% 1.7% 39.2% Transition 16.1% 211 238 1.128 - - 99 84 183 45.9% 59.6% 8.5% 5.2% 4.7% 44.1% P&R Ball Handler 9.1% 120 113 0.942 - - 51 45 96 46.9% 54.7% 16.7% 3.3% 2.5% 40.8% Hand Off 8.9% 117 111 0.949 - - 61 38 99 38.4% 49.5% 11.1% 6% 6% 36.8% Cut 5% 66 77 1.167 - - 23 35 58 60.3% 60.3% 7.6% 6.1% 6.1% 57.6% Isolation 2.8% 37 35 0.946 - - 16 12 28 42.9% 48.2% 13.5% 10.8% 10.8% 43.2% Miscellaneous 2.7% 35 19 0.543 - - 6 3 9 33.3% 38.9% 54.3% 22.9% 5.7% 28.6% """
+,""""""]
+def generate_scouting_report(data):
+    player_boxscore = random.choice(data)
+    scouting_report = get_scouting_report(client, "gpt-4o", player_boxscore, temperature, top_p, seed_value)
+    st.write(scouting_report)
+
+if st.button("Use Sample Data"):
+    generate_scouting_report(sample_data)
