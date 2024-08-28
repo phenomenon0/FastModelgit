503bf953c099c80d8b567e1eea716782f48b0fca
Femi Adeniran <66181395+phenomenon0@users.noreply.github.com>
2024-08-28 11:32:37 -0500

    Update Alpha.py
    minified innput

--- pages/Alpha.py
+++ pages/Alpha.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import json
 import streamlit as st
 import os 
-
+import pandas as pd
 
 st.markdown("# PAT and JEFFERSON 🕵️🕵️‍♂️")
 
@@ -26,50 +26,36 @@ def get_scouting_report(_llm, model, player, temperature):
     chat_completion = _llm.chat.completions.create(
         model=model,
         top_p =1,
-     
+     seed= 111,
         messages=[
             {
                     "role": "system",
-                    "content":"""You are an expert basketball scout analyzing players using box scores and synergy stats. Create a concise scouting report following these strict guidelines:
+                    "content":"""You are a basketball scout writng a report based on a players stats follow this template to report 
 
-FORMAT:
-• Use ONLY bullet points, no subheadings or paragraphs.
-• write each bullet point on a new line
-• If there is a section Spot Ups use "Spot Ups" and not Spot-ups or Spot_ups in description.
-• The synergy stats are not properly spelled sometimes e.g Spot_up or PandRBallHandler; make them more readable like Spot up, P and R Ballhandler.
-• Only mention percentiles for ORB, DRB, and FT%.
+Player Breakdown
+• [adjective] usage [Position] ([USG%] ) who plays [adjective] minutes ([MPG] mpg) scoring [PPG] PPG ([PPG] percentile) on [adjective] [TS%] TS% ([TS%] percentile)
+
+• More likely to shoot from the [outside/inside] ([2P-R or 3P-R]% [2P-R or 3P-R]) where he/she shoots a [adjective] [2P% or 3P%] [2P% or 3P%]
 
 
-Player Breakdown:
 
--  [adjective] usage [Position] ([USG%]) who plays [adjective] minutes ([MPG] mpg) scoring [PPG] PPG ([PPG] percentile) on [adjective] [TS%] TS% ([TS%] percentile) 
+• [adjective] rebounding [Position] especially on [Defense/Offense] averaging [ORB/G or DRB/G] ([DRB or ORB] percentile)
+  (Include DRB/G if > 3, ORB/G if > 1. Always mention ORB% and DRB%)
 
-- More likely to shoot from the [outside/inside] ([2P-R or 3P-R]% [2P-R or 3P-R]) where he/she shoots a [adjective] [2P% or 3P%] [2P% or 3P%] 
+• [adjective] facilitator who averages [APG] APG ([APG] percentile) with a [AST%] AST% ([AST%] percentile) and turns it over [adjective] with [TO/G] TO/G ([TO/G] percentile)
+  (Only include if USG% > 10%)
 
-- Most commonly used in synergy play type with highest % Time, where he/she produces [PPP] PPP (Never mention Miscellaneous Play type)
-  * Transition = Gets most of [his/her] shots in transition [% Time], where [he/she] produces [PPP]
-  * Post-Up = Gets most of [his/her] shots in the post [% Time], where [he/she] produces [PPP]
-  * Cut = Gets most of [his/her] shots cutting off the ball [% Time], where [he/she] produces [PPP]
-  * Spot Up = Gets most of [his/her] shots as a spot-up shooter [% Time], where [he/she] produces [PPP]
-  * P&R Ball Handler = Gets most of [his/her] shots as a ball-handler in the pick & roll [% Time] where [he/she] produces [PPP]
-  * Isolation = Gets most of [his/her] shots in isolation [% Time], where [he/she] produces [PPP]
-  * P&R Roll Man = Gets most of [his/her] shots as the roll man in the pick & roll [% Time] where [he/she] produces [PPP]
-  * Off Screen = Gets most of [his/her] shots coming around off-ball screens [% Time], where [he/she] produces [PPP]
-  * Offensive Rebounds (Put Backs) = Gets most of [his/her] shots from put backs on the offensive glass [% Time], where [he/she] produces [PPP]
-  * Handoffs = Gets most of [his/her] shots in off handoffs [% Time], where [he/she] produces [PPP]
+• [adjective] defensively, averaging [STL/G or BLK/G] [STL/G or BLK/G]
+  (Only include if STL/G or BLK/G is in 80th percentile or above)
 
-- [Second most common play type]: Also frequently gets shots in [play type] ([% Time]), producing [PPP]. 
+• [adjective] free throw shooter at [FT%] ([FT%] percentile) who gets to the line [adverb] at [FT-R] FT-R
+  (Only include if player averages more than 2 FTA per game e.g 14% FT-R)
 
-- [adjective] rebounding [Position] especially on [Defense/Offense] averaging [ORB/G or DRB/G] ([DRB or ORB] percentile) (Include DRB/G if > 3, ORB/G if > 1. Always mention ORB% and DRB%) 
 
-- [adjective] facilitator who averages [APG] APG ([APG] percentile) with a [AST%] AST% ([AST%] percentile) and turns it over [adjective] with [TO/G] TO/G ([TO/G] percentile) (Only include if USG% > 10%) 
+example
+'USG%: 14.8; MPG: 15.0; PPG: 3.9; TS%: 51.6; TS% percentile: 45.0; 3PR: 8.6; 2PR: 91.4; 3P%: 11.1; 3P% percentile: 25.0; DRB%: 19.5; DRB% percentile: 87.0; ORB%: 8.3; ORB% percentile: 77.0; AST%: 2.8; AST% percentile: 14.0; TO%: 15.4; TO% percentile: 42.0; STL/G: 0.4; BLK/G: 0.2; STL% percentile: 40.0; BLK% percentile: 53.0; STL%: 1.4; FT%: 64.7; FTRate: 32.4; FTRate percentile: 56.0'
 
-- [adjective] defensively, averaging [STL/G or BLK/G] [STL/G or BLK/G] (Only include if STL/G or BLK/G is in 80th percentile or above)//n
-
-- [adjective] free throw shooter at [FT%] ([FT%] percentile) who gets to the line [adverb] at [FT-R] FT-R (Only include if player averages more than 2 FTA per game e.g 14% FT-R)//n
-
-Analyze the provided player data and generate a breakdown strictly adhering to this format. Include only the relevant bullet points based on the player's stats and the given criteria. """
-           
+<ul>\\n <li>Low usage forward (14.8% USG) who plays average minutes (15.0 MPG) scoring 3.9 PPG on below average 51.6% TS% (45th percentile).</li>\\n \\n <li>More likely to shoot from the inside (91.4% 2P-R) where he shoots a balanced 52.1% 2P%.</li>\\n \\n <li>Gets most of his shots cutting off the ball,\<li>High rebounding forward, performing better on the defensive glass with a DRB% 19.5% (87th percentile) compared to ORB% 8.3% (77th percentile).</li>\\n \\n <li>Weak facilitator who assists on 2.8% of the team\'s field goals while on the court (14th percentile) and turns the ball over on 15.4% of their possessions (42nd percentile).</li>\\n \\n <li>Average defensively, averaging 0.4 STL/G.</li>\\n \\n <li>Weak free throw shooter at 64.7% who gets to the line frequently at 32.4% FT-R (56th percentile).</li>\\n</ul>"""
             },
             {
                 "role": "user",
@@ -90,11 +76,9 @@ if st.button("Generate Scouting Report from input"):
     st.write(scouting_report)
     
 
-# Sample data
-import json
-with open("players.json", "r") as f:
-    data = json.load(f)
-sample_data = list(data)
+#sample data mini 
+sample= pd.read_csv('small 50.csv')
+sample_data = sample.Stats
 
 def generate_scouting_report(data):
     
@@ -106,12 +90,12 @@ if st.button("Use Sample Data to Generate"):
     if "current_index" not in st.session_state:
         st.session_state.current_index = 0
     else:
-        st.session_state.current_index = (st.session_state.current_index + 1) % len(data)
+        st.session_state.current_index = (st.session_state.current_index + 1) % len(sample_data)
     player_boxscore = str(sample_data[st.session_state.current_index])
     scouting_report = get_scouting_report(client, "gpt-4o", player_boxscore, temperature)
     st.write(scouting_report)
     st.write("Data fed:" )
     st.code(sample_data[st.session_state.current_index])
-    st.session_state.current_index = (st.session_state.current_index + 1) % len(data)
+    st.session_state.current_index = (st.session_state.current_index + 1) % len(sample_data)
     
     
